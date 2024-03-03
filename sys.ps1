f5448dc42c7e926296f566ac2b1a0c5e19833835
AdamFrack <147642132+AdamFrack@users.noreply.github.com>
2024-03-03 18:53:52 +0100

    yay

---
+++ sys.ps1
@@ -0,0 +1,14 @@
+while ($true) {
+    
+    $programToRun = Read-Host "MTPWSH~"
+    if ($programToRun -eq 'quit'){
+        return 0
+    }
+    $programPath = Join-Path $PSScriptRoot "Programs" $programToRun.ToLower() "init.ps1"
+
+    if (Test-Path $programPath) {
+        & $programPath
+    } else {
+        Write-Output "$programPath not found"
+    }
+}
