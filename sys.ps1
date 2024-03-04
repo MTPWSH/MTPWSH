c17b3181781472ccf12d45d13a2f7370333d21c9
AdamFrack <adam@potatobox>
2024-03-04 22:25:57 +0100

    Committer: AdamFrack <adam@potatobox>  On branch main  Changes to be committed: 	modified:   README.md 	modified:   sys.ps1

--- sys.ps1
+++ sys.ps1
@@ -5,11 +5,13 @@ while ($true) {
     if ($programToRun -eq 'quit' -or $programToRun -eq 'exit'){
         return 0
     }
-    $programPath = Join-Path $PSScriptRoot "Programs" $programToRun.ToLower() "init.ps1"
-
-    if (Test-Path $programPath) {
-        & $programPath
+    $programPath = Join-Path $PSScriptRoot "Programs" $programToRun.ToLower()
+    $initPath = Join-Path $programPath "init.ps1"
+    if (Test-Path $initPath) {
+        & $initPath
+    } elseif (Test-Path $programPath) {
+        Write-Output "$programToRun is missing an init.ps1 file."
     } else {
-        Write-Output "$programPath not found"
+        Write-Output "$programToRun not found."
     }
 }
